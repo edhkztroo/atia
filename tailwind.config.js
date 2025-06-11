dfaf9cfe3f7a5f12a375ca803c1e9a9e22751215
edhkztroo <edhkztroo@gmail.com>
2025-06-10 23:04:03 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
 			},
 		},
 		extend: {
+			fontFamily: {
+				'sans': ['Albert Sans', 'system-ui', 'sans-serif'],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
